b85d5cd98c08c4fc0e54f9a88a4a85a8caa8d863
Andrew Szeto <andrew@jabagawee.com>
2013-10-11 18:55:05 -0700

    have an idea

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+// create the iframe
+// bind the question mark to toggle the iframe
 $(document).bind('keyup', function(e) {
     if(e.keyCode === 191 && !($(e.target).is("input") || $(e.target).is("textarea"))) {
-        console.log("go");
+        toggleIframe();
     }
 });
